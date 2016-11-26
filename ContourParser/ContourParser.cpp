47761ef82a6c10bdcfb3297c343ff0af46e9e157
tyan <tyan.evgeniy@gmail.com>
2016-11-26 16:26:10 +0300

    - profile geometry added to desc file - added test structures for holding geometry - start reading geometry

--- ContourParser/ContourParser.cpp
+++ ContourParser/ContourParser.cpp
@@ -4,8 +4,51 @@
 #include <iostream>
 #include <list>
 #include <map>
+#include <memory>
 
 #include <lua.hpp>
+
+static void stackDump (lua_State *L) 
+{
+	int i;
+	int top = lua_gettop(L);
+	for (i = 1; i <= top; i++) {  /* repeat for each level */
+		int t = lua_type(L, i);
+		switch (t) {
+
+		case LUA_TSTRING:  /* strings */
+			printf("`%s'", lua_tostring(L, i));
+			break;
+
+		case LUA_TBOOLEAN:  /* booleans */
+			printf(lua_toboolean(L, i) ? "true" : "false");
+			break;
+
+		case LUA_TNUMBER:  /* numbers */
+			printf("%g", lua_tonumber(L, i));
+			break;
+
+		default:  /* other values */
+			printf("%s", lua_typename(L, t));
+			break;
+
+		}
+		printf("  ");  /* put a separator */
+	}
+	printf("\n");  /* end the listing */
+}
+
+/* assume that table is on the stack top */
+bool getfield(lua_State *L, size_t index, double& value)
+{
+	lua_pushnumber(L, index);
+	lua_gettable(L, -2);  /* get background[key] */
+	if (!lua_isnumber(L, -1))
+		return false;
+	value = lua_tonumber(L, -1);
+	lua_pop(L, 1);  /* remove number */
+	return true;
+}
 
 /* assume that table is on the stack top */
 bool getfield(lua_State *L, const char *key, double& value)
@@ -78,7 +121,7 @@ Param readParam(lua_State *L)
 ParamsMap readParams(lua_State *L)
 {
 	ParamsMap result;
-	lua_getglobal(L, "contour_params");
+	lua_getglobal(L, "profile_params");
 
 	lua_pushnil(L);  /* first key */
 	while (lua_next(L, 1) != 0) {
@@ -87,6 +130,9 @@ ParamsMap readParams(lua_State *L)
 		/* removes 'value'; keeps 'key' for next iteration */
 		lua_pop(L, 1);
 	}
+
+	lua_pop(L, 1);
+
 	return result;
 }
 
@@ -101,6 +147,104 @@ void showParams(const ParamsMap& params)
 	}
 }
 
+struct Point
+{
+	double x;
+	double y;
+};
+
+typedef std::list<Point> PointArray;
+
+Point readPoint(lua_State *L)
+{
+	Point result;
+	if(getfield(L, 1, result.x) == false || getfield(L, 2, result.y))
+		throw "point reading error";
+	return result;
+}
+
+class IGeometryItem
+{
+public:
+	virtual ~IGeometryItem(){}
+};
+
+typedef std::shared_ptr<IGeometryItem> IGeometryItemPtr;
+typedef std::list<IGeometryItemPtr> IGeometryItemPtrArray;
+
+class Polygon : public IGeometryItem
+{
+public:
+	PointArray m_points;
+};
+
+PointArray readPointArray(lua_State *L)
+{
+	PointArray result;
+	
+	lua_pushnil(L);  /* first key */
+	while (lua_next(L, 1) != 0) {
+		/* uses 'key' (at index -2) and 'value' (at index -1) */
+		result.push_back(readPoint(L));
+		/* removes 'value'; keeps 'key' for next iteration */
+		lua_pop(L, 1);
+	}
+	return result;
+}
+
+std::shared_ptr<Polygon> readPolygon(lua_State *L)
+{
+	std::shared_ptr<Polygon> result(new Polygon());
+	
+	lua_pushstring(L, "points");
+	lua_gettable(L, -2);  /* get background[key] */
+	
+	
+
+	lua_pop(L, 1);  /* remove number */
+
+	return result;
+}
+
+IGeometryItemPtr readGeometryItem(lua_State *L)
+{
+	std::string typeName;
+	if(!getfield(L, "type", typeName))
+		throw "error while reading ";
+	if(typeName == "polygon")
+	{
+		return readPolygon(L);
+	}
+	else
+	{
+		throw "unknown geometry item type";
+	}
+}
+
+class Profile
+{
+public:
+	IGeometryItemPtrArray m_items;
+};
+
+Profile readProfile(lua_State *L)
+{
+	Profile result;
+	lua_getglobal(L, "profile_geometry");
+
+	lua_pushnil(L);  /* first key */
+	while (lua_next(L, 1) != 0) {
+		/* uses 'key' (at index -2) and 'value' (at index -1) */
+		result.m_items.push_back(readGeometryItem(L));
+		/* removes 'value'; keeps 'key' for next iteration */
+		lua_pop(L, 1);
+	}
+
+	lua_pop(L, 1);
+
+	return result;
+}
+
 int main (void) {
 	//char buff[256];
 	//int error;
@@ -113,17 +257,21 @@ int main (void) {
 	if (0 != lua_pcall(L, 0, 0, 0))
 		std::cout << "Cannot run file!\n";
 
-	auto params = readParams(L);
+	try
+	{
+		stackDump(L);
 
-	showParams(params);
-	//while (fgets(buff, sizeof(buff), stdin) != NULL) {
-	//	error = luaL_loadbuffer(L, buff, strlen(buff), "line") ||
-	//		lua_pcall(L, 0, 0, 0);
-	//	if (error) {
-	//		fprintf(stderr, "%s", lua_tostring(L, -1));
-	//		lua_pop(L, 1);  /* pop error message from the stack */
-	//	}
-	//}
+		auto params = readParams(L);
+		//showParams(params);
+
+		stackDump(L);
+
+		auto geometry = readProfile(L);
+	}
+	catch(const char* msg)
+	{
+		std::cout << msg;
+	}
 
 	lua_close(L);
 	return 0;
