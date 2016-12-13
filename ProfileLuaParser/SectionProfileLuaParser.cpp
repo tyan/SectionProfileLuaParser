117a198e85f3a852d0682160d756ad48c6c3e462
tyan <tyan.evgeniy@gmail.com>
2016-12-13 23:34:24 +0300

    - this works for update - update function added to script

--- ProfileLuaParser/SectionProfileLuaParser.cpp
+++ ProfileLuaParser/SectionProfileLuaParser.cpp
@@ -7,21 +7,41 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <functional>
 
-int main (void) {
-	lua_State *L= luaL_newstate();   /* opens Lua */
+//TODO: remove global variable
+std::shared_ptr<Profile> g_profile;
+
+int rengaUpdateParams (lua_State *L) {
+  if(g_profile)
+  {
+    g_profile->m_params["W"].defaultValue = 3000;
+    updateProfileParams(*g_profile, L);
+  }
+  return 0;
+}
+
+std::shared_ptr<Profile> readProfile()
+{
+  lua_State *L= luaL_newstate();   /* opens Lua */
 	luaL_openlibs(L);             /* opens the basic library */
+
+  std::shared_ptr<Profile> result;
 
   try
   {
     if (0 != luaL_loadfile(L, "double_tee_beam.rpfl"))
       throw "Cannot find file!\n";
 
+    lua_pushcfunction(L, rengaUpdateParams);
+    lua_setglobal(L, "rengaUpdateParams");
+
     if (0 != lua_pcall(L, 0, 0, 0))
       throw "Cannot run file!\n";
 
     auto profile = readProfile(L);
     outputProfile(profile, std::cout);
+    result.reset(new Profile(profile));
 	}
 	catch(const char* msg)
 	{
@@ -29,5 +49,44 @@ int main (void) {
 	}
 
 	lua_close(L);
+  return result;
+}
+
+void writeProfile(const Profile& profile)
+{
+  lua_State *L= luaL_newstate();   /* opens Lua */
+	luaL_openlibs(L);             /* opens the basic library */
+
+  try
+  {
+    if (0 != luaL_loadfile(L, "double_tee_beam.rpfl"))
+      throw "Cannot find file!\n";
+    
+    lua_pushcfunction(L, rengaUpdateParams);
+    lua_setglobal(L, "rengaUpdateParams");
+
+    g_profile.reset(new Profile(profile));
+
+    if (0 != lua_pcall(L, 0, 0, 0))
+      throw "Cannot run file!\n";
+
+    auto profile = readProfile(L);
+    outputProfile(profile, std::cout);
+
+    g_profile = nullptr;
+	}
+	catch(const char* msg)
+	{
+		std::cout << msg;
+	}
+
+	lua_close(L);
+}
+
+int main (void) 
+{
+  auto profile = readProfile();
+  if(profile)
+    writeProfile(*profile);
 	return 0;
 }
